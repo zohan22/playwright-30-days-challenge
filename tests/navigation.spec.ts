406b8760e339ad152bf5000db22322debfd3a2ff
Samuel Amonzabel <amonzabels@gmail.com>
2026-06-12 18:07:50 -0400

    third challenge

---
+++ tests/navigation.spec.ts
@@ -0,0 +1,41 @@
+import { test, expect } from '@playwright/test';
+
+test('Check left menu option', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
+
+  const leftMenuItems = page.getByRole('navigation', { name: 'Sidepanel'}).getByRole('listitem')
+  const currentMenuItemsCount = await leftMenuItems.count();
+  console.log('Current menu items count: ', currentMenuItemsCount);
+
+  const currentMenuItems: string[] = [];
+
+  for(let i=0; i<currentMenuItemsCount; i++) {
+    const menuText = await leftMenuItems.nth(i).innerText();
+    currentMenuItems.push(menuText);
+  }
+
+  console.log('Current menu items: ', currentMenuItems);
+
+  const expectedMenuItems = [
+    'Admin',
+    'PIM',
+    'Leave',
+    'Time',
+    'Recruitment',
+    'My Info',
+    'Performance',
+    'Dashboard',
+    'Directory',
+    'Maintenance',
+    'Claim',
+    'Buzz'
+  ]
+
+  expect(currentMenuItems).toEqual(expectedMenuItems);
+  console.log('First menu item: ', currentMenuItems[0]);
+  expect(currentMenuItems[0]).toBe('Admin');
+})
