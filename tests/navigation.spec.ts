0abf14f8e662e1967f3691272e23d62b1924512f
Samuel Amonzabel <amonzabels@gmail.com>
2026-06-12 18:31:54 -0400

    fourth challenge

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -7,13 +7,13 @@ test('Check left menu option', async ({ page }) => {
   await page.getByRole('button', { name: 'Login' }).click();
   await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
 
-  const leftMenuItems = page.getByRole('navigation', { name: 'Sidepanel'}).getByRole('listitem')
+  const leftMenuItems = page.getByRole('navigation', { name: 'Sidepanel' }).getByRole('listitem')
   const currentMenuItemsCount = await leftMenuItems.count();
   console.log('Current menu items count: ', currentMenuItemsCount);
 
   const currentMenuItems: string[] = [];
 
-  for(let i=0; i<currentMenuItemsCount; i++) {
+  for (let i = 0; i < currentMenuItemsCount; i++) {
     const menuText = await leftMenuItems.nth(i).innerText();
     currentMenuItems.push(menuText);
   }
@@ -38,4 +38,29 @@ test('Check left menu option', async ({ page }) => {
   expect(currentMenuItems).toEqual(expectedMenuItems);
   console.log('First menu item: ', currentMenuItems[0]);
   expect(currentMenuItems[0]).toBe('Admin');
+})
+
+test('Navigate through the left panel', async ({ page }) => {
+  test.setTimeout(60000);
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
+
+  const leftMenuItems = page.getByRole('navigation', { name: 'Sidepanel' }).getByRole('listitem')
+  const currentMenuItemsCount = await leftMenuItems.count();
+
+  for(let i=0; i<currentMenuItemsCount; i++) {
+    const menuItem = leftMenuItems.nth(i)
+
+    const menuText = await menuItem.innerText()
+    await menuItem.click();
+
+    console.log('Clicking on menu item: ', menuText);
+    if(menuText === 'Maintenance') {
+      await page.goBack();
+    }
+  }
+
 })
